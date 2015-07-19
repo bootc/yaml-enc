e5906539bdbec9ef1f876d5738472e69de7f5ffb
Chris Boot <bootc@bootc.net>
2015-07-19 14:00:34 +0100

    Major refactoring.
    Create a Node class so that we can re-use it later, and use
    collections.ChainMap to handle merging other YAML files more easily.
    Also implement a custom PyYAML representer for the Node class, and
    output the mapping in a defined order for consistency with Puppet
    Dashboard.
    
    The output format is now much closer to Puppet Dashboard's.

--- yaml-enc.py
+++ yaml-enc.py
@@ -22,158 +22,154 @@ import os
 import sys
 import yaml
 
+from collections import ChainMap, OrderedDict
+
+
 DEFAULT_DIR = '/etc/puppet/yaml-enc'
 NODES_SUBDIR = 'nodes'
 
 
-def read_yaml(base_dir, path):
-    """
-    Read a YAML file, handling any processing directives.
+class Node(object):
+    def __init__(self, base_dir, fqdn, classes={}, parameters={},
+                 environment=None):
+        super(Node, self).__init__()
+        self.base_dir = base_dir
+        self.fqdn = fqdn
+        self.classes = ChainMap(classes)
+        self.parameters = ChainMap(parameters)
+        self.environment = environment
 
-    Currently only handles a ``:include`` directive, which is used to include
-    other YAML files into the current data dictionary.
-    """
-    # Construct the path to the YAML file
-    path_yaml = os.path.join(base_dir, path + '.yaml')
+    @classmethod
+    def from_yaml(cls, base_dir, fqdn, Loader=yaml.SafeLoader):
+        node = cls(base_dir, fqdn)
+        node.merge_yaml(os.path.join(NODES_SUBDIR, fqdn), Loader=Loader)
+        return node
 
-    # Read the YAML data
-    with open(path_yaml, 'r') as fd:
-        data = yaml.safe_load(fd)
+    @classmethod
+    def all_nodes(cls, base_dir, Loader=yaml.SafeLoader):  # generator
+        nodes_dir = os.path.join(base_dir, NODES_SUBDIR)
 
-    # Handle any :include directives
-    if ':include' in data:
-        includes = data[':include']
-        del data[':include']
+        for (_, _, filenames) in os.walk(nodes_dir):
+            for fqdn in (x[:-5] for x in filenames if x.endswith('.yaml')):
+                yield Node.from_yaml(base_dir, fqdn, Loader=Loader)
 
-        # The :include value could be a string or a list; if it's a string,
-        # convert it to a list for ease
-        if isinstance(includes, str):
-            includes = [includes]
+            # Break out of the walk; we only want to walk the nodes_dir
+            break
 
-        # Merge in all included data
-        for inc in includes:
-            merge(data, read_yaml(base_dir, inc))
+    def merge_yaml(self, path, Loader=yaml.SafeLoader):
+        # Construct the path to the YAML file
+        path_yaml = os.path.join(self.base_dir, path + '.yaml')
 
-    return data
+        # Read the YAML data
+        with open(path_yaml, 'r') as fd:
+            loader = Loader(fd)
+            try:
+                data = loader.get_single_data()
+            finally:
+                loader.dispose()
 
+        # Handle empty node definitions gracefully
+        if data is None:
+            return
 
-def merge(main, inc):
-    """
-    Merge ENC data from ``inc`` into ``main``.
+        # We can only cope with dicts in the YAML
+        if not isinstance(data, dict):
+            print("E: {path}: YAML document must have a mapping at its top "
+                  "level".format(path=path_yaml), file=sys.stderr)
+            sys.exit(1)
 
-    This does a context-sensitive merge for data that may be returned to Puppet
-    from an ENC. Generally, when including a YAML file, the values in the
-    including file override the values from the included file.
-
-    The following keys are merged:
-
-    ``classes``:
-      Any classes present in ``inc`` but *not* already present in ``main`` are
-      merged into ``main``. This method handles class arguments, but argument
-      lists are *not* merged.
-
-    ``parameters``:
-      Any parameters present in ``inc`` but *not* already present in ``main``
-      are merged into ``main``. Parameter values are *not* merged.
-
-    ``environment``:
-      If ``main`` does not specify an environment but ``inc`` does, the
-      environment value is merged.
-    """
-    # Handle merging classes
-    if 'classes' in inc:
-        if 'classes' not in main:
-            main['classes'] = inc['classes']
+        # Add the included classes
+        try:
+            classes = data['classes']
+        except KeyError:
+            pass
         else:
-            left = main['classes']
-            right = inc['classes']
+            if isinstance(classes, list):
+                classes = dict.fromkeys(classes)
+            self.classes.maps.append(classes)
 
-            # If both are a list, the result should still be a list
-            if isinstance(left, list) and isinstance(right, list):
-                seen = set(left)
-                seen_add = seen.add
-                # Add unique items from right to left
-                left.extend(x for x in right if not (x in seen or seen_add(x)))
+        # Add the included parameters
+        try:
+            self.parameters.maps.append(data['parameters'])
+        except KeyError:
+            pass
+
+        # Set the environment
+        try:
+            if self.environment is None:
+                self.environment = data['environment']
+        except KeyError:
+            pass
+
+        # Handle any :include directives
+        try:
+            includes = data[':include']
+        except KeyError:
+            pass
+        else:
+            # The :include value could be a string or a list
+            if isinstance(includes, str):
+                self.merge_yaml(includes)
             else:
-                # One or the other may be a list, so convert them to dict
-                if isinstance(left, list):
-                    left = dict.fromkeys(left, None)
-                elif isinstance(right, list):
-                    right = dict.fromkeys(right, None)
+                for inc in includes:
+                    self.merge_yaml(inc)
 
-                # Add classes from right to left only if they don't already
-                # exist in left
-                left.update({k: right[k] for k in right if k not in left})
+    def _flatten(self):
+        flattened = OrderedDict(name=self.fqdn)
 
-    # Handle merging parameters
-    if 'parameters' in inc:
-        if 'parameters' not in main:
-            main['parameters'] = inc['parameters']
+        if not any(self.classes.values()):
+            # If none of our classes have any arguments, use a list instead
+            flattened['classes'] = list(self.classes.keys())
         else:
-            left = main['parameters']
-            right = inc['parameters']
+            # Flatten our list of classes
+            flattened['classes'] = dict(self.classes)
 
-            # Add parameters from right to left only if they don't already
-            # exist in left
-            left.update({k: right[k] for k in right if k not in left})
+        # Flatten our list of parameters
+        flattened['parameters'] = dict(self.parameters)
 
-    # Handle merging environment
-    if 'environment' in inc and 'environment' not in main:
-        main['environment'] = inc['environment']
+        if self.environment is not None:
+            flattened['environment'] = self.environment
+
+        return flattened
+
+    @classmethod
+    def add_yaml_representer(cls, dumper=yaml.SafeDumper):
+        def _node_yaml_representer(dumper, node):
+            return dumper.represent_mapping(
+                yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+                node._flatten().items())
+        dumper.add_representer(cls, _node_yaml_representer)
 
 
-def classify_node(base_dir, fqdn):
-    return read_yaml(base_dir, os.path.join(NODES_SUBDIR, fqdn))
-
-
-def classify_all_nodes(base_dir):
-    nodes_dir = os.path.join(base_dir, NODES_SUBDIR)
-    nodes = []
-
-    for (_, _, filenames) in os.walk(nodes_dir):
-        for fqdn in (x[:-5] for x in filenames if x.endswith('.yaml')):
-            data = classify_node(base_dir, fqdn)
-            data['name'] = fqdn
-            nodes.append(data)
-
-        # Break out of the walk; we only want to walk the nodes_dir
-        break
-
-    return nodes
+# Teach PyYAML how to represent Node objects
+Node.add_yaml_representer()
 
 
 def main():
     parser = argparse.ArgumentParser(
         description='YAML-based Puppet External Node Classifier (ENC)')
-    parser.add_argument('node', nargs='?',
-                        help='FQDN of the node to classify')
+    parser.add_argument('FQDN', nargs='?',
+                        help='hostname of the node to classify')
     parser.add_argument('--base', '-b', default=DEFAULT_DIR,
                         help='base directory for node YAML files')
     args = parser.parse_args()
 
     # Check that the base directory exists and is a directory
     if not os.path.isdir(args.base):
-        print("E: {base} does not exist or is not a directory"
-              .format(base=args.base), file=sys.stderr)
-        sys.exit(1)
-
-    # Check that the nodes directory exists and is a directory
-    nodes_dir = os.path.join(args.base, NODES_SUBDIR)
-    if not os.path.isdir(nodes_dir):
-        print("E: {nodes} does not exist or is not a directory"
-              .format(nodes=nodes_dir), file=sys.stderr)
+        print("E: {} does not exist or is not a directory".format(args.base),
+              file=sys.stderr)
         sys.exit(1)
 
     try:
-        if args.node is not None:
-            data = classify_node(args.base, args.node)
+        if args.FQDN is not None:
+            data = Node.from_yaml(args.base, args.FQDN)
         else:
-            data = classify_all_nodes(args.base)
+            data = list(Node.all_nodes(args.base))
     except OSError as e:
         print("E: {e.filename}: {e.strerror}".format(e=e), file=sys.stderr)
         sys.exit(1)
-
-    yaml.safe_dump(data, sys.stdout, explicit_start=True)
+    else:
+        yaml.safe_dump(data, sys.stdout, explicit_start=True)
 
 if __name__ == "__main__":
     main()
